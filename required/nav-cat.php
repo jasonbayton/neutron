afd1ac6fb1e02209a0f0f74789d2726dc678d4e6
Jason Bayton <jason@bayton.org>
2016-10-18 22:00:54 +0000

    progress on the sidenav, broken category list to be fixed

--- required/nav-cat.php
+++ required/nav-cat.php
@@ -1,12 +1,12 @@
 <?php
-$path = $settings ["sitepath"] . '/content/' . $pwd;
+$path = $settings ["sitepath"] . '/content';
 		
                 function recursive_menu( $path, $pwd, $settings ){
                 $number = 1;
-		$number++;
 		$dirs = glob( $path . "/*");
 		$realfile = glob( $path."/*.md" );
 			foreach ($realfile as $ri){
+				$number++;
 				$slug = substr($ri,0,-3);
 		                $subfile = glob( $slug . "/*.md");
 				$ricontent = file_get_contents($ri);
@@ -16,12 +16,13 @@ $path = $settings ["sitepath"] . '/content/' . $pwd;
         			$ri_settings ["contenttype"] = ucfirst($ri_settings ["contenttype"]);
 				echo '<ul class="nav-content-base">';
 				if ($ri_settings ["contenttype"]=='Category'){
-				echo '<li><a data-toggle="collapse" data-target="sub-'.$number.'">'
+				echo '<li><a data-toggle="collapse" data-target="#sub'.$number.'">'
         			.$ri_settings ["title"].'<span class="caret"></span></a>'
         			.'</li>';
-				}
-				echo '<div id="sub-'.$number.'" class="collapse">';
+				};
+				echo '<div id="sub'.$number.'" class="collapse">';
 				foreach ($subfile as $si){
+					$number++;
 					$subslug = substr($si,0,-3);
 					$sub_subfile = glob( $subslug . "/*.md");
 					echo '<ul class="nav-category-indent">';
@@ -31,13 +32,15 @@ $path = $settings ["sitepath"] . '/content/' . $pwd;
                                 	$si_settings = $si_raw->getYAML();
                                 	$si_settings ["contenttype"] = ucfirst($si_settings ["contenttype"]);
 					if ($si_settings ["contenttype"]=='Category'){	
-					echo '<li><a data-toggle="collapse" data-target="#subsubmenu">'
+					echo '<li><a data-toggle="collapse" data-target="#subsub-'.$number.'">'
                                 	.$si_settings ["title"].'<span class="caret"></span></a>'
                                 	.'</li>';
 					}
-					echo '<div id="subsubmenu" class="collapse">';
+					echo '<div id="subsub-'.$number.'" class="collapse">';
 					foreach ($sub_subfile as $mi){
+						$number++;
 						$sub_subslug = substr($mi,0,-3);
+						$subsub_subfile = glob( $sub_subslug . "/*.md");
 						echo '<ul class="nav-category-indent">';
 						$micontent = file_get_contents($mi);
                                 		$parser = new Mni\FrontYAML\Parser();
@@ -45,14 +48,31 @@ $path = $settings ["sitepath"] . '/content/' . $pwd;
                                 		$mi_settings = $mi_raw->getYAML();
                                 		$mi_settings ["contenttype"] = ucfirst($mi_settings ["contenttype"]);
 						if ($mi_settings ["contenttype"]=='Category'){
-						echo '<li><a href="'.($settings ["siteurl"]).'/'. $pwd . '/'
-                                		.basename($slug).'/'
-						.basename($subslug).'/'
-						.basename($sub_subslug).'">'
-                                		.$mi_settings ["title"].'</a>'
+						echo '<li><a data-toggle="collapse" data-target="#subsubsub-'.$number.'">'
+                                        	.$mi_settings ["title"].'<span class="caret"></span></a>'
                                 		.'</li>';
 						}
-						echo '</ul>';
+						echo '<div id="subsubsub-'.$number.'" class="collapse">';
+                                        	foreach ($subsub_subfile as $ci){
+                                                	$number++;
+                                                	$subsub_subslug = substr($ci,0,-3);
+                                                	echo '<ul class="nav-category-indent">';
+                                                	$cicontent = file_get_contents($ci);
+                                                	$parser = new Mni\FrontYAML\Parser();
+                                                	$ci_raw = $parser->parse($cicontent);
+                                                	$ci_settings = $mi_raw->getYAML();
+                                                	$ci_settings ["contenttype"] = ucfirst($ci_settings ["contenttype"]);
+                                                	if ($ci_settings ["contenttype"]=='Category'){
+                                                	echo '<li><a data-toggle="collapse" data-target="#subsubsub-'.$number.'">'
+                                                	.$ci_settings ["title"].'<span class="caret"></span></a>'
+                                                	.'</li>';
+                                                	}
+
+							echo '</ul>';
+						};
+	                                        echo '</div>';
+	                                        echo '</ul>';
+
 					};
 					echo '</div>';
                        			echo '</ul>';
